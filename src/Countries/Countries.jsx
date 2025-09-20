54ffb440722c3f8c358465be536fd9886d69c1ec
sakhawat236hossain <hmdsakhawat236@gmail.com>
2025-09-20 23:18:24 +0600

    secound

--- src/Countries/Countries.jsx
+++ src/Countries/Countries.jsx
@@ -9,9 +9,10 @@ const Countries = ({CountriesPromise}) => {
  return(
     <div>
         <h2>totle countries: {countries.length}</h2>
+        <h2>visited countrise:</h2>
       <div  className='countries'> 
           {
-            countries.map(country=><Country country={country}></Country>)
+            countries.map(country=><Country key={country.cca3.cca3} country={country}></Country>)
         }
       </div>
     </div>
--- src/Country/Country.jsx
+++ src/Country/Country.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import './Country.css'
-const Country = ({country}) => {
-    console.log(country);
-    return (
-        <div className='country'>
-            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
-            <h3>name: {country.name.common}</h3>
-        </div>
-    );
+import React from "react";
+import "./Country.css";
+const Country = ({ country }) => {
+  console.log(country.area.area);
+  return (
+    <div className="country">
+      <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+      <h3>
+        name: {country.name.common} ({country.cca3.cca3})
+      </h3>
+      <h3>capital: {country.capital.capital[0]}</h3>
+      <h3>continents: {country.continents.continents[0]}</h3>
+      <h3>population: {country.population.population}</h3>
+      <h3>region: {country.region.region}</h3>
+      <h3>area: {country.area.area} {country.area.area>300000?"big country":"small country"}</h3>
+    </div>
+  );
 };
 
 export default Country;
